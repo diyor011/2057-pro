6409f04c07a010d9c5b905d4d986d4d075ddfd82
diyor011 <diyorraxmatullaev3@gmail.com>
2025-12-15 11:23:52 +0500

    ssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Weather from './pages/Wether.jsx'
+import TodoList from './pages/Todolist.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
             <Weather/>  
+            <TodoList/>
     </div>
 
 
