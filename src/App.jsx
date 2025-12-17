444b3175b70ed29ffbc4af346d9c792f109d3405
sarvarabriyev016-create <sarvarabriyev016@gmail.com>
2025-12-17 15:59:12 +0500

    product

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <Navbar/>
     
         <section className='flex h-fullr w-full '>
-
+        
              <Sidebar/>
                <Outlet/>
         </section>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -10,17 +10,33 @@ const Product = () => {
 
         } catch (err) {
             console.error(err);
-            
+
         }
     }
     useEffect(() => {
         GetProduct()
     }, [])
-  return (
-    <div>
-     
-    </div>
-  )
+    return (
+         <div className='flex flex-wrap justify-center gap-5'>
+            {product.map(item => (
+                <div key={item.id} className="card bg-base-100 w-96 shadow-sm">
+                    <figure>
+                        <img
+                            src={item.thumbnail}
+                            alt={item.title}
+                        />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{item.title}</h2>
+                        <p>{item.description}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Product
